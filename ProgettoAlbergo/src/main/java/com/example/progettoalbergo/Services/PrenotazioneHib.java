e4a725168b0ba762e79b62fdea5bc89d3664fb06
Itsihe11 <ihe@studenti.laziodigital.it>
2026-07-17 17:34:23 +0200

    wipprenotazionehib

--- ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Services/PrenotazioneHib.java
+++ ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Services/PrenotazioneHib.java
@@ -1,6 +1,8 @@
 package com.example.progettoalbergo.Services;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,9 +11,13 @@ import org.springframework.stereotype.Service;
 
 
 import com.example.progettoalbergo.Model.Prenotazione;
+import com.example.progettoalbergo.Model.PrenotazioneStanza;
 import com.example.progettoalbergo.Model.Stanza;
 import com.example.progettoalbergo.Repository.PrenotazioneRepository;
+import com.example.progettoalbergo.Repository.PrenotazioneStanzaRepository;
 import com.example.progettoalbergo.Repository.StanzaRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class PrenotazioneHib {
@@ -19,16 +25,16 @@ public class PrenotazioneHib {
     private StanzaRepository stanzaRepository;
 	
 	@Autowired
-	private PrenotazioneRepository PrenotazioneRepository;
+	private PrenotazioneRepository prenotazioneRepository;
 	
+
 	public List<Prenotazione> getAllPrenotazione() {
-		return PrenotazioneRepository.findAll();
+		return prenotazioneRepository.findAll();
 	}
 	
 	
-	
 	public void cancellaPrenotazione(String codice) {
-		PrenotazioneRepository.deleteById(codice);
+		prenotazioneRepository.deleteById(codice);
 	}
 	
 	
@@ -36,6 +42,36 @@ public class PrenotazioneHib {
     public List<Stanza> getStanzeDisponibili(LocalDate checkIn, LocalDate checkOut) {
       
         return stanzaRepository.findStanzeDisponibili(checkIn, checkOut);
+    }
+    
+    @Transactional
+    public Prenotazione creaPrenotazione(Long idStanza, LocalDate checkIn, LocalDate checkOut, String pensione, BigDecimal pensioneCosto ) {
+    	
+    	List<Stanza> libere = stanzaRepository.findStanzeDisponibili(checkIn, checkOut);
+    	
+    	boolean isDisponibile= libere.stream().anyMatch(s-> s.getId().equals(idStanza));
+    	
+    	Stanza stanza = stanzaRepository.findById(idStanza)
+                .orElseThrow(() -> new IllegalArgumentException("Stanza non trovata con ID: " + idStanza));
+
+       
+        Prenotazione prenotazione = new Prenotazione();
+        
+
+        
+        PrenotazioneStanza dettaglio = new PrenotazioneStanza();
+        dettaglio.setStanza(stanza);
+        dettaglio.setCheckin(checkIn);
+        dettaglio.setCheckout(checkOut);
+        dettaglio.setPensione(pensione);
+        dettaglio.setCostoPensione(costoPensione);
+
+       
+        prenotazione.addStanza(dettaglio);
+
+        
+        return prenotazioneRepository.save(prenotazione);
+    
     }
 	
 
