bf920b04e728825e497d879fd70216cd4af49025
Itsihe11 <ihe@studenti.laziodigital.it>
2026-07-17 14:43:33 +0200

    controllostanza

--- ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Repository/PrenotazioneRepository.java
+++ ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Repository/PrenotazioneRepository.java
@@ -1,5 +1,11 @@
 package com.example.progettoalbergo.Repository;
 
-public class PrenotazioneRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.progettoalbergo.Model.Prenotazione;
+
+@Repository
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, String>{
 
 }
