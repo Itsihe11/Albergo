bfa471d8eec54ac6852cb363555661e15104a3c2
RiccardoByte <riccardob807@gmail.com>
2026-07-19 19:05:24 +0200

    get e post su tipologia stanze

--- ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Controller/TipologiaStanzaController.java
+++ ProgettoAlbergo/src/main/java/com/example/progettoalbergo/Controller/TipologiaStanzaController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-
+import com.example.progettoalbergo.Model.TipologiaStanza;
+import com.example.progettoalbergo.Services.TipologiaStanzaHib;
 
 @RestController
 @RequestMapping("/api/dipendente/tipologie")
 public class TipologiaStanzaController {
 
+	@Autowired
+	TipologiaStanzaHib TipologiaDependency;
+
+	@GetMapping("/tipologieStanze")
+	public List<TipologiaStanza> listaTipologie() {
+
+		List<TipologiaStanza> lista = TipologiaDependency.getAllTipologiaStanze();
+
+		return lista;
+
+	}
+	
+	@PostMapping("/datiStanza")
+	public String inserisciDati(@RequestBody TipologiaStanza nuovaTipologia) {
+		
+		TipologiaDependency.addTipologiaStanza(nuovaTipologia);
+
+		
+		return "tipologia inserita";
+		
+	}
 
 }
